07440c98b835865e73bf1d55f85651a1ee8f3cf9
Joseph Herlant <herlantj@gmail.com>
2017-11-15 11:05:32 -0800

    Add tests for CloudFrontProcessor.GetTags

--- cloudfront_test.go
+++ cloudfront_test.go
@@ -29,7 +29,8 @@ func (m *mockCloudFrontClient) TagResource(input *cloudfront.TagResourceInput) (
 }
 
 func (m *mockCloudFrontClient) ListTagsForResource(input *cloudfront.ListTagsForResourceInput) (*cloudfront.ListTagsForResourceOutput, error) {
-	return nil, m.ReturnError
+	m.ResourceID = input.Resource
+	return &cloudfront.ListTagsForResourceOutput{Tags: m.ResourceTags}, m.ReturnError
 }
 
 func TestSetTag(t *testing.T) {
@@ -61,6 +62,37 @@ func TestSetTag(t *testing.T) {
 
 		if !reflect.DeepEqual(*mockSvc.ResourceTags, *d.outputTag) {
 			t.Errorf("Expecting to update tag: %v\nGot: %v\n", *d.outputTag, *mockSvc.ResourceTags)
+		}
+	}
+}
+
+func TestGetTags(t *testing.T) {
+	testData := []struct {
+		inputResource, outputResource string
+		inputTags                     *cloudfront.Tags
+		outputTags                    []*cloudfront.Tag
+		inputError, outputError       error
+	}{
+		{"my resource", "my resource", &cloudfront.Tags{Items: []*cloudfront.Tag{}}, []*cloudfront.Tag{}, nil, nil},
+		{"my resource", "my resource", &cloudfront.Tags{Items: []*cloudfront.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}}}, []*cloudfront.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}}, nil, nil},
+		{"my resource", "my resource", &cloudfront.Tags{Items: []*cloudfront.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}, {Key: aws.String("Aerosmith"), Value: aws.String("rocks")}}}, []*cloudfront.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}, {Key: aws.String("Aerosmith"), Value: aws.String("rocks")}}, nil, nil},
+		{"my resource", "my resource", &cloudfront.Tags{Items: []*cloudfront.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}}}, []*cloudfront.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}}, errors.New("Badaboom"), errors.New("Badaboom")},
+	}
+	for _, d := range testData {
+		mockSvc := &mockCloudFrontClient{ReturnError: d.inputError, ResourceTags: d.inputTags}
+		p := CloudFrontProcessor{svc: mockSvc}
+
+		res, err := p.GetTags(&d.inputResource)
+		if !reflect.DeepEqual(err, d.outputError) {
+			t.Errorf("Expecting error: %v\nGot: %v\n", d.outputError, err)
+		}
+
+		if *mockSvc.ResourceID != d.outputResource {
+			t.Errorf("Expecting resource: %s, got: %s\n", d.outputResource, *mockSvc.ResourceID)
+		}
+
+		if !reflect.DeepEqual(res, d.outputTags) {
+			t.Errorf("Expecting to get tags: %v\nGot: %v\n", d.outputTags, res)
 		}
 	}
 }

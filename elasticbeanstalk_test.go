45d99d0ba8bc414fafa1f0681b9fc312d292884c
Joseph Herlant <herlantj@gmail.com>
2017-11-23 19:00:14 -0800

    Add test for ElasticBeanstalkProcessor.TagsToMap

---
+++ elasticbeanstalk_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/elasticbeanstalk"
+	"reflect"
+	"testing"
+)
+
+func TestElasticBeanstalkTagsToMap(t *testing.T) {
+	testData := []struct {
+		inputTags  []*elasticbeanstalk.Tag
+		outputTags map[string]string
+	}{
+		{[]*elasticbeanstalk.Tag{}, map[string]string{}},
+		{[]*elasticbeanstalk.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}}, map[string]string{"foo": "bar"}},
+		{[]*elasticbeanstalk.Tag{{Key: aws.String("foo"), Value: aws.String("bar")}, {Key: aws.String("Aerosmith"), Value: aws.String("rocks")}}, map[string]string{"foo": "bar", "Aerosmith": "rocks"}},
+	}
+	for _, d := range testData {
+		p := ElasticBeanstalkProcessor{}
+
+		res := p.TagsToMap(d.inputTags)
+		if !reflect.DeepEqual(res, d.outputTags) {
+			t.Errorf("Expecting to get tags: %v\nGot: %v\n", d.outputTags, res)
+		}
+	}
+}

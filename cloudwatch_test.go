10a6ac538e12d7ebed2ad70bfb1b9cde02a67c70
Joseph Herlant <herlantj@gmail.com>
2017-12-05 21:59:59 -0800

    Add a few unit tests

---
+++ cloudwatch_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+)
+
+func TestCwTagsToMap(t *testing.T) {
+	testData := []struct {
+		inputTags  map[string]*string
+		outputTags map[string]string
+	}{
+		{map[string]*string{}, map[string]string{}},
+		{map[string]*string{"foo": aws.String("bar")}, map[string]string{"foo": "bar"}},
+		{map[string]*string{"foo": aws.String("bar"), "Aerosmith": aws.String("rocks")}, map[string]string{"foo": "bar", "Aerosmith": "rocks"}},
+	}
+	for _, d := range testData {
+		p := CwProcessor{}
+
+		res := p.TagsToMap(d.inputTags)
+		if !reflect.DeepEqual(res, d.outputTags) {
+			t.Errorf("Expecting to get tags: %v\nGot: %v\n", d.outputTags, res)
+		}
+	}
+}
